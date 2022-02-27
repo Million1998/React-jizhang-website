85a42e5b77431d0425f63b97846764bc03574e91
million1998 <1194396343@qq.com>
2022-02-27 10:45:14 +0800

    添加了svg

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
   Link
 } from 'react-router-dom';
 import styled from 'styled-components';
+import Nav from './components/Nav';
 
 const Wrapper = styled.div`
   border:1px solid red;
@@ -15,20 +16,8 @@ const Wrapper = styled.div`
   flex-direction: column;
 `;
 const Main = styled.div`
-  border:1px solid green;
   flex-grow: 1;
   overflow:auto;
-`;
-const Nav = styled.nav`
-  border:1px solid bule;
-  > ul {
-    display:flex;
-    > li{
-      width:33.333%;
-      text-align:center;
-      padding:16px;
-    }
-   }
 `;
 
 function App() {
@@ -52,19 +41,7 @@ function App() {
             </Route>
           </Switch>
         </Main>
-        <Nav>
-          <ul>
-            <li>
-              <Link to="/tags">标签页</Link>
-            </li>
-            <li>
-              <Link to="/money">记账页</Link>
-            </li>
-            <li>
-              <Link to="/statistics">统计页</Link>
-            </li>
-          </ul>
-        </Nav>
+        <Nav/>
       </Wrapper>
     </Router>
   );
