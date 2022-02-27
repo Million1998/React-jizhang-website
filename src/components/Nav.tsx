98e7d5cfeebcdfc1c95195928340cfcb7b28a95b
million1998 <1194396343@qq.com>
2022-02-27 12:45:36 +0800

    完成三个 icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+import 'icons/tag.svg';
+import 'icons/money.svg';
+import 'icons/chart.svg';
+
 
 const NavWrapper = styled.nav`
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -9,8 +13,11 @@ const NavWrapper = styled.nav`
     display:flex;
     > li{
       width:33.333%;
+      padding: 2px 0;
       text-align:center;
-      padding:16px;
+      display:flex;
+      flex-direction: column;
+      align-items:center;
     }
    }
 `;
@@ -20,17 +27,26 @@ const Nav = () =>{
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon" >
+            <use xlinkHref="#tag" />
+          </svg>
           <Link to="/tags">标签页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money" />
+          </svg>
           <Link to="/money">记账页</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#chart" />
+          </svg>
           <Link to="/statistics">统计页</Link>
         </li>
       </ul>
     </NavWrapper>
   )
-}
+};
 
 export default Nav;
