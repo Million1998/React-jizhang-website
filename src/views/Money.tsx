56eefed8e231e9f67d16164199afcf85baf8d7b0
million1998 <1194396343@qq.com>
2022-03-01 15:18:41 +0800

    完成备注

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,7 +1,7 @@
 import Layout from '../components/Layout';
 import React from 'react';
 import {CategorySection} from './Money/CategorySection';
-import {NotesSection} from './Money/NotesSection';
+import {NoteSection} from './Money/NoteSection';
 import {NumberPadSection} from './Money/NumberPadSection';
 import {TagsSection} from './Money/TagsSection';
 /*const MyLayout = styled(Layout)`
@@ -14,12 +14,7 @@ function Money() {
   return (
     <Layout>
       <TagsSection/>
-      <NotesSection>
-        <label>
-          <span>备注</span>
-          <input type="text" placeholder="在这里添加备注"/>
-        </label>
-      </NotesSection>
+      <NoteSection/>
       <CategorySection>
         <ul>
           <li className="selected">支出</li>
