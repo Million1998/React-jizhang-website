e6c186bdc470e63b6330c442663be124816286d3
million1998 <1194396343@qq.com>
2022-03-01 12:30:56 +0800

    完成标签的选择和新增

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  border:1px solid red;
   height: 100vh;
   display: flex;
   flex-direction: column;
 `;
 const Main = styled.div`
+  display: flex;
+  flex-direction: column;
   flex-grow: 1;
   overflow:auto;
 `;
@@ -16,7 +17,7 @@ const Main = styled.div`
 const Layout = (props: any) => {
   return (
     <Wrapper>
-      <Main>
+      <Main /*className={props.className}*/>
         {props.children}
       </Main>
       <Nav/>
