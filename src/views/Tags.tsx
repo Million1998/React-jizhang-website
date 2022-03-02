896762cb0951fd1527c09348456d9fedb3258461
million1998 <1194396343@qq.com>
2022-03-02 15:40:45 +0800

    创建 /tags/:tag 路由，学会 exact 的用法

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -3,21 +3,24 @@ import React from 'react';
 import {useTags} from '../useTags';
 import styled from 'styled-components';
 import Icon from '../components/Icon';
+import {Link} from 'react-router-dom';
 
 const TagList = styled.ol`
   font-size: 16px;
   >li{
   border-bottom: 1px solid #d5d5d9;
-  padding: 12px 16px 12px 0;
   margin-left: 16px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+    >a{
+    padding: 12px 16px 12px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
   }
 `;
 const Button = styled.button`
   font-size: 18px;border: none;padding: 8px 12px;
-  background: #767676;border-radius: 4px;
+  background: #f60;border-radius: 4px;
   color:white;
 `;
 const Center = styled.div`
@@ -34,8 +37,10 @@ function Tags() {
       <TagList>
         {tags.map(tag =>
           <li key={tag}>
+            <Link to="/tags/:tag">
             <span className="oneLine">{tag}</span>
             <Icon name="right"/>
+            </Link>
           </li>
         )}
       </TagList>
